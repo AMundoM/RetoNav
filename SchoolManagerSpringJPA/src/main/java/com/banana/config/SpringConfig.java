9ce7f58fb26925ff1cadd2f2709717f9a44ac263
ricardo <ricardo@enmotionvalue.com>
2024-01-05 12:35:25 +0100

    upddate

--- SchoolManagerSpringJPA/src/main/java/com/banana/config/SpringConfig.java
+++ SchoolManagerSpringJPA/src/main/java/com/banana/config/SpringConfig.java
@@ -1,12 +1,14 @@
 package com.banana.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({PropertiesConfig.class, PropertiesConfigDev.class, PersistenceConfig.class, ReposConfig.class, ServicesConfig.class})
+@Import({PropertiesConfig.class, PropertiesConfigDev.class, /*PersistenceConfig.class,*/ ReposConfig.class, ServicesConfig.class})
 @ComponentScan(basePackages = {"com.banana.persistence", "com.banana.services"})
+@EntityScan(basePackages = {"com.banana.models"}) // for scaning entities when emf bean no set and using properties
 public class SpringConfig {
 
 }
